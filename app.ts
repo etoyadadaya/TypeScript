f5ca8049e9dfa22fe2dcc8fd6c6584fbbe1fe68c
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-21 16:56:55 +0700

    json type

--- app.ts
+++ app.ts
@@ -1,16 +1,16 @@
 const user = {
-  firstName: "Johan",
-  secondName: "Lie",
-  city: "Moscow",
-  age: 20,
-  skills: {
-    dev: true,
-    devOps: true
-  }
+    firstName: "Johan",
+    secondName: "Lie",
+    city: "Moscow",
+    age: 20,
+    skills: {
+        dev: true,
+        devOps: true
+    }
 }
 
 function getFullName(userEntity: { firstName: string, secondName: string }): string {
-  return `${userEntity.firstName} ${userEntity.secondName}`;
+    return `${userEntity.firstName} ${userEntity.secondName}`;
 }
 
 console.log(getFullName(user));
---
+++ test.ts
@@ -0,0 +1,21 @@
+let info: {
+    officeId: number;
+    isOpened: boolean;
+    contacts: {
+        phone: string;
+        email: string;
+        address: {
+            city: string;
+        }
+    }
+} = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+8800-555-35-35",
+        "email": "credit@gmail.com",
+        "address": {
+            "city": "Moscow"
+        }
+    }
+}
