313f5ac9bf5c94be75a90389a591436424a4cde1
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-21 17:23:42 +0700

    readonly property

--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
-const skills: [number, string] = [1, "Dev"];
+const skills: [number, string] = [1, "Dev"]; // two types tuple
 
-const [id, skillName] = skills;
+const [id, skillName] = skills; // destructive assignment
 
-const arr: [number, string, ...boolean[]] = [1, "DevOps", true, true, false];
+const arr: readonly [number, string, ...boolean[]] = [1, "DevOps", true, true, false]; // readonly tuple
+
+const someSkills: readonly string[] = ["Dev", "DevOps"]; // readonly string array
+
+const extraSkills: ReadonlyArray<string> = ["Dev", "DevOps"]; // readonly generic
+
+
