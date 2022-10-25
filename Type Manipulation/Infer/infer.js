c3e5ffc642d703dc241bcb50260b18490dba6e1b
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-25 14:47:18 +0700

    infer logic

---
+++ Type Manipulation/Infer/infer.js
@@ -0,0 +1,8 @@
+"use strict";
+function runTransaction(transaction) {
+    console.log(transaction);
+}
+const transaction = {
+    fromTo: ['1', '2'],
+};
+runTransaction(transaction);
---
+++ Type Manipulation/Infer/infer.ts
@@ -0,0 +1,13 @@
+function runTransaction(transaction: {
+    fromTo: [string, string]
+}) {
+    console.log(transaction);
+}
+
+const transaction: GetFirstArg<typeof runTransaction> = {
+    fromTo: ['1', '2'], // as [string, string] - working, easy way
+};
+
+runTransaction(transaction);
+
+type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any ? First : never;
